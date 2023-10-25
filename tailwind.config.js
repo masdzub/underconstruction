563e3c437b22076e0ab7a031fff93d3527c6d7d9
Dzubayyan Ahmad <masdzub@gmail.com>
2023-10-26 00:11:57 +0700

    upload to git

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      './public/index.html',
+    ],
+    // ...
+    theme: {
+        extend: {
+          backgroundColor: {
+            'dark': '#1A202C',
+          },
+          textColor: {
+            'dark': '#E5E7EB',
+          },
+        },
+      },
+  }
